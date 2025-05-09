8eaca9759342056c152cda6f527e25bf14fa0b47
sadafmaheen19@gmail.com <maheensadaf@Maheensadafs-MacBook-Air-2.local>
2025-05-09 21:38:27 +0530

    Added usermanagement files

---
+++ src/test/java/org/jsp/UserManagementSys/UserManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.UserManagementSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
